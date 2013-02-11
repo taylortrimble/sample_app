ea933823384c194bbb66cbe12c4e99b2571e7701
Taylor Trimble <t.trim@me.com>
2013-02-10 19:02:30 -0600

    Add fancy flashing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Thank you for signing up for Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
