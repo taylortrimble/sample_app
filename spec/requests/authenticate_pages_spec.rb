b6462408b4225a67a3264312f01e1afa750894e4
Taylor Trimble <t.trim@me.com>
2013-02-11 22:42:38 -0600

    Create a Sessions controller & add routes

---
+++ spec/requests/authenticate_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Authenticate" do
+  
+  subject { page }
+  
+  describe "sign in page" do
+    before { visit signin_path }
+    
+    it { should have_selector('h1', text: "Sign in") }
+    it { should have_selector('title', text: "Sign in") }
+  end
+end
