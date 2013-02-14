6b6fd2d9e473cb7f932bbbd2e12e11dc8c59fc68
Taylor Trimble <t.trim@me.com>
2013-02-14 12:26:09 -0600

    Fix and refactor tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -103,10 +103,6 @@ describe User do
     
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
-      
-      describe "after saving the user" do
-        it { should have_link('Sign out') }
-      end
     end
     
     describe "with invalid password" do
