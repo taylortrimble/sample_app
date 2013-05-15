792ed48382fdc74c12d5095fc5bb5e7c5174719b
Taylor Trimble <t.trim@me.com>
2013-05-15 14:30:37 -0500

    Check for wrong user attempting edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   
   def new
     @user = User.new
@@ -22,11 +23,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    # User retrieved by correct_user
   end
   
   def update
-    @user = User.find(params[:id])
+    # User retrieved by correct_user
     if @user.update_attributes(params[:user])
       sign_in @user
       flash[:success] = "Profile updated"
@@ -40,5 +41,10 @@ class UsersController < ApplicationController
   
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in" unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
     end
 end
