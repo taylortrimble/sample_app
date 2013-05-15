d07a7ae7b64b9bd569c09aa2ff0dd78e19e29360
Taylor Trimble <t.trim@me.com>
2013-05-14 21:25:42 -0500

    Make settings work!

--- spec/requests/authenticate_pages_spec.rb
+++ spec/requests/authenticate_pages_spec.rb
@@ -26,6 +26,7 @@ describe "Authenticate" do
     
       it { should have_full_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
